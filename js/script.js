5b02cd3f12279bc706aa17b8833df0d561db07b8
Everton Pila Silva <84790670+Everton-Pila-Silva@users.noreply.github.com>
2021-09-15 18:57:04 -0300

    Change background and others configs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
 //-------------------------------------------------------------------------------//
+$(function(){ 
+    var navMain = $(".navbar-collapse"); // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
 
 
 function regressiva(){
